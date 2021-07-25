fed1a742fbcc0b8fec29c0cc78e1e4efbb3cfe29
Junho <u_innovation@naver.com>
2021-07-25 19:44:02 +0900

    Core

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,15 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    @Test
+    void join() {
+        // given ~이러 이러한게 주어졌을 때
+
+        // when ~했을 때
+
+        // then ~이렇게 된다 !
+    }
+}
