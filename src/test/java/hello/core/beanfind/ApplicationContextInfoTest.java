f4304c078d057e2e6686bba61b09e6984794243c
Junho <u_innovation@naver.com>
2021-07-30 02:42:11 +0900

    Bean 출력하기

---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,41 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) { // iter + Tap
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("beanDefinitionName = " + beanDefinitionName + "object" + bean);
+        }
+    }
+
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) { // iter + Tab
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName); // Bean 하나 하나에 대한 정보
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("beanDefinitionName = " + beanDefinitionName + "Object" + bean);
+
+            }
+
+
+
+        }
+    }
+}
